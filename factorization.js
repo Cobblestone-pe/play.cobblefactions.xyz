405a02eff358872a2baa6451cc86917cab4c7761
mwear3robloxandroid <mitchellwears@gmail.com>
2016-12-12 02:36:36 +0000

    Update factorization.js

--- factorization.js
+++ factorization.js
@@ -1,4 +1,4 @@
-Block.defineBlock(180, "CustomBlock", ["iron", 0], );
+Block.defineBlock(180, "CustomBlock", ["iron", 0], 3, true, 7);
 Block.setDestroyTime(180, 0.3);
 
 Block.setShape(180, 2/16, 0, 2/16, 14/16, 1, 14/16);
